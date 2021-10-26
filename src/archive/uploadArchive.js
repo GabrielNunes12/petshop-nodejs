5344c06535ee42dff13c97a4071cd4468a56ee03
Gabriel Nunes <prismaticnetwork12@gmail.com>
2021-10-25 22:01:10 -0300

    fix: preparing file to create a new image based on Buffer

--- src/archive/uploadArchive.js
+++ src/archive/uploadArchive.js
@@ -5,7 +5,10 @@ fs.readFile(`${path.dirname(__dirname)}/assets/Grooming.jpg`, (erro, buffer) => 
   if(erro) {
     throw new Error('Error while buffering the image');
   } else {
-    console.log('imagem foi bufferizada')
-    console.log(buffer)
+    console.log(buffer);
+    // creating a file called by name of salcichaPeludinha.jpg
+    fs.writeFile(`${path.dirname(__dirname)}/assets/salcichaPeludinha.jpg`, buffer, (error)=> {
+      console.log('Image created');
+    })
   }
 })
